33a7632487cb0fdc429dbf5d848c3ea36e757f75
Emadram <refemad@outlook.com>
2025-03-19 18:17:14 +0200

    Updated

--- cyptrip-frontend/src/App.jsx
+++ cyptrip-frontend/src/App.jsx
@@ -6,12 +6,13 @@ import AiChat from './pages/AiChat';
 import Deals from './pages/Deals';
 import Explore from './pages/Explore';
 import TravelGuide from './pages/TravelGuide';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
 import AboutUs from './pages/AboutUs';
-import Team from './pages/Team';
-import FAQs from './pages/FAQs';
-import ContactUs from './pages/ContactUs';
+// import ContactUs from './pages/ContactUs';
+import SignIn from './pages/Signin';
+import SignUp from './pages/Signup';
+import OurTeam from './pages/OurTeam';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import TermsConditions from './pages/TermsConditions';
 
 function App() {
   return (
@@ -21,13 +22,15 @@ function App() {
         <Route path="/ai-chat" element={<AiChat />} />
         <Route path="/deals" element={<Deals />} />
         <Route path="/explore" element={<Explore />} />
+        <Route path="/explore/:id" element={<Explore />} />
         <Route path="/travel-guide" element={<TravelGuide />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/faqs" element={<FAQs />} />
-        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/team" element={<OurTeam />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/terms" element={<TermsConditions />} />
       </Routes>
     </BrowserRouter>
   );
